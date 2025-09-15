169e5e1aaf6957bb60553f5fbbacc96a9d3c04f0
xlaedri <128313635+xlaedri@users.noreply.github.com>
2025-09-15 18:50:22 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,32 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    unoptimized: true
+  },
+  // Önemli: Eğer static export yapacaksanız bu satırı ekleyin
+  // output: 'export',
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          }
+        ],
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
