737d4ea8b54d478544ac1df583f16eb344686ea3
xlaedri <128313635+xlaedri@users.noreply.github.com>
2025-09-16 00:03:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div style={{ padding: '50px', fontFamily: 'Arial' }}>
-      <h1>BySerwerus Kütüphanesi</h1>
+      <h1>BySerwerus DigiTalk Kütüphanesi</h1>
       <p>Hoş geldiniz! Site yapım aşamasındadır.</p>
       <p>Yakında 3D kitaplar ve sosyal özelliklerle hizmetinizde olacağız.</p>
     </div>
