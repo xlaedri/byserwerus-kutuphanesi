5c13e0d3e8ad1e442e776defa3c6b6667a868400
xlaedri <128313635+xlaedri@users.noreply.github.com>
2025-09-16 20:33:28 +0200

    Create books.js

---
+++ books.js
@@ -0,0 +1,20 @@
+export const books = [
+  {
+    id: 1,
+    title: "Sineklerin Tanrısı",
+    author: "William Golding",
+    imageUrl: "https://i.dr.com.tr/cache/600x600-0/originals/0000000064552-1.jpg" 
+  },
+  {
+    id: 2,
+    title: "Yabancı",
+    author: "Albert Camus",
+    imageUrl: "https://img.kitapyurdu.com/v1/getImage/fn:11382158/wh:true/wi:800"
+  },
+  {
+    id: 3,
+    title: "Kürk Mantolu Madonna",
+    author: "Sabahattin Ali",
+    imageUrl: "https://img.kitapyurdu.com/v1/getImage/fn:11455593/wh:true/wi:800"
+  }
+];
